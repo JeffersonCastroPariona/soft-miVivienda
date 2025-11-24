d1b01b3bc08af16fbb7ce28b0dde3553b687c4e2
JeffersonCastroPariona <74164641+JeffersonCastroPariona@users.noreply.github.com>
2025-11-24 02:39:32 -0500

    Update changes soft

--- src/test/java/com/upc/deporte/ClinicaApplicationTests.java
+++ src/test/java/com/upc/deporte/ClinicaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.upc.deporte;
 
+import com.upc.ep.EpApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EpApplication.class)
 class ClinicaApplicationTests {
 
     @Test
